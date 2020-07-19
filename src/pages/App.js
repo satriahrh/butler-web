b4fe66b1e83350890606495a6131573f1bcfdc79
Satria H R Harsono <satriahrh@gmail.com>
2020-07-19 09:21:08 +0700

    greeting

--- src/pages/App.js
+++ src/pages/App.js
@@ -1,15 +1,40 @@
-import React from "react";
+import React, {useState, useEffect} from "react";
 import {useOktaAuth} from '@okta/okta-react'
 import Page from "./Page";
-
+import {Alert, Container} from "react-bootstrap";
 
 export default function App() {
+  const [user, setUser] = useState();
+  const [userIsLoaded, setUserIsLoaded] = useState(false);
+  const [error, setError] = useState();
   const {authService} = useOktaAuth();
+  useEffect(() => {
+    if (!userIsLoaded) {
+      setUserIsLoaded(true);
+      authService.getUser(
+      ).then(
+        res => {
+          setUser(res)
+        }
+      ).catch(
+        err => {
+          setError(err.message)
+        }
+      );
+    }
+  });
+
+  const errorAlert = error && <Alert variant='danger'>error</Alert>;
   return (
     <Page appContexts={{
-      logoutCallback: () => {authService.logout('/')}
+      logoutCallback: () => {
+        authService.logout('/')
+      }
     }}>
-      <h1>You are logged in!</h1>
+      <Container>
+        {errorAlert}
+        <h1>Hello, {user && user.given_name}</h1>
+      </Container>
     </Page>
   )
 }
